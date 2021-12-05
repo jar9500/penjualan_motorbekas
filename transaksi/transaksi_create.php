2b95c3a7cc2968a3466874d5a758e3119df15053
19122_Rico Senjaya <84189632+rich-s3n@users.noreply.github.com>
2021-12-06 03:21:06 +0700

    Adding Process(Still Error)
    CRUD were failed to run, something error on the code itself. (Puyeng gan)

---
+++ transaksi/transaksi_create.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+session_start(); 
+?>
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="../style.css">
+<script src="../bootstrap/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<!-- navbar -->
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <div class="container-fluid">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a href="../index.php"><img src="../img/logo.png" class="logo-navbar"><a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="identitas_motor.php">Katalog Motor</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="transaksi.php">Transaksi</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Kelola User</a>
+        </li>
+    </ul>
+    </div>
+    <?php if (isset($_SESSION["Nama"])) : ?>
+        <p class="h6 text-white mr-5">Halo,<?php echo $_SESSION["Nama"] ?></p>
+        <a href="../logout.php" class="btn btn-light buttonnavbar">Logout</a>
+    <?php else : ?>
+        <a href="../login.php" class="btn btn-light buttonnavbar">Login</a>
+    <?php endif; ?>
+</nav>
+<!-- header -->
+<div class="header">
+    <h1 class="mt-5">Input Data Transaksi</h1>
+</div>
+<!-- body -->
+<div class="container" >
+		<div class="bg-light m-4 p-4">
+			<form method="post" action="identitas_motor_createproses.php">
+                <div class="form-group row my-4">
+                    <label class="col-sm-2 col-form-label" for="ID">ID</label>
+                    <div class="col-sm-4">
+                        <input type="number" class="form-control" name="ID" placeholder="ID">
+                    </div>
+                </div>
+                <div class="form-group row my-4">
+                    <label class="col-sm-2 col-form-label" for="NoRegistrasi">No Registrasi</label>
+                    <div class="col-sm-4">
+                        <input type="number" class="form-control" name="NoRegistrasi" placeholder="No Registrasi">
+                    </div>
+                </div>
+                <div class="form-group row my-4">
+                    <label class="col-sm-2 col-form-label" for="NamaPemilik">Nama Pemilik</label>
+                    <div class="col-sm-4">
+                        <input type="text" class="form-control" name="NamaPemilik" placeholder="Nama Pemilik">
+                    </div>
+                </div>
+                
+                <div class="form-group row my-4">
+                    <div class="col-sm-10">
+                        <input type="submit" class="btn btn-success px-4" name="Simpan"></button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        Hanya Pemilik yang bisa akses halaman ini, Selamat Datang <?php echo $_SESSION["Nama"] ?>
+    </div>
+    <!-- Footer -->
+<footer>
+    <h6 class="text-center p-5"> @Copyright - JDRB 2021</h6>
+</footer>
+</body>
+
+</html>
