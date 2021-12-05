6d3afa702c6ffa46e313fd600ea80bb73f3b7942
19126-RizkyBayuDwiputra <rizky.bayu19126@student.unsika.ac.id>
2021-12-06 01:45:40 +0700

    Multi level login

--- loginproses.php
+++ loginproses.php
@@ -1,18 +1,69 @@
 <?php
+
+// mengaktifkan session pada php
 session_start();
 
+// menghubungkan php dengan koneksi database
 include 'koneksi.php';
-$Nama=$_POST['Nama'];
-$Password=$_POST['Password'];
-$query=mysqli_query($koneksi,"SELECT * FROM user WHERE Nama='$Nama' AND Password=md5('$Password')")
-or die(mysqli_error($koneksi));
 
-if ($data=mysqli_fetch_array($query)){
-    $_SESSION["Nama"]=$data["Nama"];
-    $_SESSION["alert"]="BERHASIL LOGIN";
-    header("Location:index.php");
+// menangkap data yang dikirim dari form login
+$Nama = $_POST['Nama'];
+$Password = $_POST['Password'];
+
+
+// menyeleksi data user dengan username dan password yang sesuai
+$query = mysqli_query($koneksi,"SELECT * FROM user WHERE Nama='$Nama' AND Password=md5('$Password')")
+or die(mysqli_error($koneksi));
+// menghitung jumlah data yang ditemukan
+
+$cek = mysqli_num_rows($query);
+
+// cek apakah username dan password di temukan pada database
+if($cek > 0){
+
+	$data = mysqli_fetch_assoc($query);
+
+	// cek jika user login sebagai pemilik
+	if($data['Hak_Akses']=="Pemilik"){
+
+		// buat session login dan username
+		$_SESSION['Nama'] = $Nama;
+		$_SESSION['Hak_Akses'] = "Pemilik";
+		// alihkan ke halaman dashboard admin
+		header("location:home/pemilik.php");
+
+	// cek jika user login sebagai Teller
+	}else if($data['Hak_Akses']=="Teller"){
+		// buat session login dan username
+		$_SESSION['Nama'] = $Nama;
+		$_SESSION['Hak_Akses'] = "Teller";
+		// alihkan ke halaman dashboard pegawai
+		header("location:home/teller.php");
+
+	// cek jika user login sebagai Customer
+	}else if($data['Hak_Akses']=="Customer"){
+		// buat session login dan username
+		$_SESSION['Nama'] = $Nama;
+		$_SESSION['Hak_Akses'] = "Customer";
+		// alihkan ke halaman dashboard pengurus
+		header("location:home/customer.php");
+
+    }else if($data['Hak_Akses']=="Teknisi"){
+		// buat session login dan username
+		$_SESSION['Nama'] = $Nama;
+		$_SESSION['Hak_Akses'] = "Teknisi";
+		// alihkan ke halaman dashboard pengurus
+		header("location:home/teknisi.php");
+
+	}else{
+
+		// alihkan ke halaman login kembali
+		header("location:index.php?pesan=gagal");
+	}	
 }else{
-    $_SESSION["alert"]="Username/Password Salah";
-    header("Location:login.php");
+	header("location:index.php?pesan=gagal");
 }
+
+?>
+
 ?>
