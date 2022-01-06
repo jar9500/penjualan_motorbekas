0e55bc8ccb3f26555b39266833cf45fc01cdc8f5
19126-RizkyBayuDwiputra <rizky.bayu19126@student.unsika.ac.id>
2022-01-06 11:54:00 +0700

    Ganti judul gaes

--- home/Pemilik/identitas_motor.php
+++ home/Pemilik/identitas_motor.php
@@ -12,7 +12,7 @@ if( !isset($_SESSION['Pemilik']) )
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Document</title>
+<title>Halaman Identitas Motor</title>
 <link rel="stylesheet" href="../../bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="../../style.css">
 <script src="../../bootstrap/js/bootstrap.min.js"></script>
